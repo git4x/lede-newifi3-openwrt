78247b26827cb215968d6236591576830cba38d7
silmace <62671124+silmace@users.noreply.github.com>
2020-03-27 03:01:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,8 +11,10 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-th
 sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-# OpenClash
-git clone https://github.com/frainzy1477/luci-app-clash.git package/mine/luci-app-clash
+#Clash
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#修复Clash随OpenWrt官方源码一起编译出现的依赖冲突
+sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
 # 管控上网行为
 git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
 # disable usb3.0
