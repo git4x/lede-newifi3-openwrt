6a8ae237dc15bc6470d5a76dda74c57f594d3be0
silmace <62671124+silmace@users.noreply.github.com>
2020-03-27 22:08:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,12 +6,15 @@
 # Blog: https://p3terx.com
 #=================================================
 # 更改默认主题为Argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
+rm -r package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
 sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #Clash
-#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#修复Clash随OpenWrt官方源码一起编译出现的依赖冲突
+sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
 # 管控上网行为
 git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
 # disable usb3.0
