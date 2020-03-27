abe575f7902f2d275515250aa9317f19d8636d6b
silmace <62671124+silmace@users.noreply.github.com>
2020-03-27 22:10:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -14,7 +14,7 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #Clash
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 #修复Clash随OpenWrt官方源码一起编译出现的依赖冲突
-sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
+#sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
 # 管控上网行为
 git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
 # disable usb3.0
