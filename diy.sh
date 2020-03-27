e1856855122afad8f7f7aeca3660e9bed2e361eb
silmace <62671124+silmace@users.noreply.github.com>
2020-03-28 04:12:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,9 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # 更改默认主题为Argon
-rm -r package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+#rm -r package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #Clash
