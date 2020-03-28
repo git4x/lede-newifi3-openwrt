960b82e25fc62fce29d4b924fd164d05d42f09f1
silmace <62671124+silmace@users.noreply.github.com>
2020-03-28 11:04:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #修复Clash随OpenWrt官方源码一起编译出现的依赖冲突
 #sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
 # 更改默认主题为Argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
 sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 # OpenClash
