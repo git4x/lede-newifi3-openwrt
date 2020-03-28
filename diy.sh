c9adef7423aa07d115b8e28fc57509203cd8ccb8
silmace <62671124+silmace@users.noreply.github.com>
2020-03-28 10:51:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,8 +9,8 @@
 #rm -r package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
-sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #Clash
