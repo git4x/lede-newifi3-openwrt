d10cf38690f112b1d590e2c5f4943777e96b66eb
silmace <62671124+silmace@users.noreply.github.com>
2020-03-28 15:36:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,11 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# 更改默认主题为Argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-cd lede/package/lean  
-rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #Clash
@@ -20,6 +15,11 @@ sed -i 's/+luci +luci-base //' package/luci-app-clash/Makefile
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # disable usb3.0
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/luci-app-usb3disable
+# 更改默认主题为Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+cd package/lean  
+rm -rf luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
