8b380fbee2afb07f4b4af3c2a01abcdb78b37995
silmace <62671124+silmace@users.noreply.github.com>
2020-04-17 10:46:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -27,7 +27,7 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/luci-app-usb3disable
 
 #其它自定义软件
-git clone https://github.com/kenzok8/openwrt-packages.git package
+git clone https://github.com/kenzok8/openwrt-packages.git  ./package
 
 # 更改默认主题为Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
