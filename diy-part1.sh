ad3b01fd60ec213f50609e16771dd8173196383e
diligent-boy <62671124+silmace@users.noreply.github.com>
2020-06-15 16:52:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@ sed -i 's/OpenWrt/newifi3/g' package/base-files/files/bin/config_generate
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# Openclash
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# Clash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/Clash
 
 # AdGuardHome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
@@ -27,16 +27,16 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/luci-app-usb3disable
 
 #ssr-plus
-#cat >  feeds.conf.default <<EOF
-#src-git helloworld https://github.com/fw876/helloworld
-#EOF
+cat >  feeds.conf.default <<EOF
+src-git helloworld https://github.com/fw876/helloworld
+EOF
 # 其它自定义软件
 #git clone https://github.com/kenzok8/openwrt-packages.git
 #mv openwrt-packages/luci-app-passwall package/
 #rm -rf openwrt-packages
 
 # 更改luci-theme-bootstrap为Argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #rm -rf package/lean/luci-theme-argon  
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
 
