c78e6bdc91d9794f6287113499e399f5cc55333d
diligent-boy <62671124+silmace@users.noreply.github.com>
2020-06-22 16:23:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,9 +27,10 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/luci-app-usb3disable
 
 #ssr-plus
-cat >  feeds.conf.default <<EOF
-src-git helloworld https://github.com/fw876/helloworld
-EOF
+#echo "src-git helloworld https://github.com/fw876/helloworld" >>feeds.conf.default
+#cat >  feeds.conf.default <<EOF
+#src-git helloworld https://github.com/fw876/helloworld
+#EOF
 # 其它自定义软件
 #git clone https://github.com/kenzok8/openwrt-packages.git
 #mv openwrt-packages/luci-app-passwall package/
