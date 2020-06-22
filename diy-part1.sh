c3c3e2fa5cebe17ee016478c02c4f2c6f7e41442
diligent-boy <62671124+silmace@users.noreply.github.com>
2020-06-22 15:35:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 git clone https://github.com/frainzy1477/luci-app-clash.git package/Clash
 
 # AdGuardHome
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # KPR plus+
 #git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
@@ -37,8 +37,8 @@ EOF
 
 # 更改luci-theme-bootstrap为Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-#rm -rf package/lean/luci-theme-argon  
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  
 
 
 ./scripts/feeds update -a
