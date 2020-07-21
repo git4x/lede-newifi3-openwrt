30368cde5f6c690370af16ea64238ab095e2d920
diligent-boy <62671124+silmace@users.noreply.github.com>
2020-07-22 03:57:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,7 @@ git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/luci
 
 #ssr-plus
 echo "src-git helloworld https://github.com/fw876/helloworld" >>feeds.conf.default
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 
 # 其它自定义软件
 #git clone https://github.com/kenzok8/openwrt-packages.git
